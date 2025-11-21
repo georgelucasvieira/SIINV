1bd60086315a75574853de0a49bb44c5cd4a51e5
georgelucasvieira <george.vieira.carmo@gmail.com>
2025-11-21 14:32:17 -0300

    Fix - swagger e limite de memoria sql server

--- src/API_Investimentos/API_Investimentos.Api/Program.cs
+++ src/API_Investimentos/API_Investimentos.Api/Program.cs
@@ -173,15 +173,12 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Investimentos v1");
-        c.RoutePrefix = string.Empty; // Swagger na raiz
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Investimentos v1");
+    c.RoutePrefix = string.Empty;
+});
 
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
